886b7ce7f67c1e20915c757cf9b90359127a33de
Douglas Memdes <capudougmor@gmail.com>
2020-04-08 17:20:31 -0300

    created page ideas

--- scripts.js
+++ scripts.js
@@ -4,6 +4,20 @@ function onOff() {
     .querySelector("#modal")//procura um elemento
     .classList//adiciona uma class
     .toggle("hide")//alterna entre os estilos
+
+    document
+    .querySelector("body")
+    .classList
+    .toggle("hideScroll")
+
+    document
+    .querySelector("#modal")
+    .classList
+    .toggle("addScroll")
+}
+
+function newIdeia() {
+    
 }
 /*segundo metodo
 document
